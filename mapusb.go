2dda9b460c44a91ae8f800cf17dc83e0f34144b7
Enrico <enrico.bison@spindoxlabs.it>
2019-11-11 11:42:39 +0100

    fix map configuration

--- mapusb.go
+++ mapusb.go
@@ -1,11 +1,19 @@
 package map_usb_devices
 
 import (
+	"encoding/json"
 	"errors"
 	"sort"
 	"strconv"
 	"strings"
 )
+
+type DeviceInfo struct {
+	Manufacturer string `json:"manufacturer"`
+	Product      string `json:"product"`
+	SerialNumber string `json:"serialNumber`
+	Description  string `json:"description"`
+}
 
 func GetUsbDeviceMap(root, dev string, product, vendor uint16) (map[string]string, error) {
 
@@ -28,10 +36,17 @@ func GetUsbDeviceMap(root, dev string, product, vendor uint16) (map[string]strin
 	tempMap := make(map[int]string, len(pDevices))
 	var orderKeys []int
 	for _, dev := range pDevices {
-		iAddr := strings.LastIndex(dev, "addr=")
-		if iAddr != -1 {
-			split := strings.Split(dev[iAddr:], "=")
-			kAddr, _ := strconv.Atoi(split[1])
+		deviceInfo := new(DeviceInfo)
+		err := json.Unmarshal([]byte(dev), deviceInfo)
+		if err != nil {
+			return nil, err
+		}
+		split := strings.Split(deviceInfo.Description, "=")
+		if len(split) > 0 {
+			kAddr, err := strconv.Atoi(split[len(split)-1])
+			if err != nil {
+				return nil, err
+			}
 			tempMap[kAddr] = dev
 			orderKeys = append(orderKeys, kAddr)
 		}
